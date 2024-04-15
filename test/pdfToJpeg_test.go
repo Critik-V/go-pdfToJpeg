e3d868e78768a65af6dbef7c709574836c0bf8f6
yannick2009 <yannickkouakou13@gmail.com>
2024-04-15 10:59:23 +0200

    fix: defer as well the images folders

--- test/pdfToJpeg_test.go
+++ test/pdfToJpeg_test.go
@@ -15,22 +15,18 @@ const url string = "http://localhost:5001/convert"
 
 func TestConvertPdfToJpeg(t *testing.T) {
 
-	// t.Run("convert one pdf to jpeg", func(t *testing.T) {
-	// 	defer func() {
-	// 		require.NoError(t, os.RemoveAll("../images"))
-	// 	}()
-	// 	body := []byte(`{"filename": "test"}`)
-	// 	res, err := http.Post(url, "application/json", bytes.NewReader(body))
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 	}
-	// 	require.Equal(t, http.StatusOK, res.StatusCode)
-	// })
+	defer os.RemoveAll("../images")
+
+	t.Run("convert one pdf to jpeg", func(t *testing.T) {
+		body := []byte(`{"filename": "test"}`)
+		res, err := http.Post(url, "application/json", bytes.NewReader(body))
+		if err != nil {
+			t.Error(err)
+		}
+		require.Equal(t, http.StatusCreated, res.StatusCode)
+	})
 
 	t.Run("convert multiple pdf to jpeg", func(t *testing.T) {
-		defer func() {
-			require.NoError(t, os.RemoveAll("../images"))
-		}()
 		var wg sync.WaitGroup
 		for i := 0; i < 4; i++ {
 			wg.Add(1)
@@ -42,31 +38,29 @@ func TestConvertPdfToJpeg(t *testing.T) {
 				if err != nil {
 					t.Error(err)
 				}
-				require.Equal(t, http.StatusOK, res.StatusCode)
+				require.Equal(t, http.StatusCreated, res.StatusCode)
 			}()
 		}
 		wg.Wait()
 	})
-	// t.Run("convert inexistent pdf to jpeg", func(t *testing.T) {
-	// 	body := []byte(`{"filename": "___.pdf"}`)
-	// 	res, err := http.Post(url, "application/json", bytes.NewReader(body))
-	// 	if err != nil {
-	// 		t.Error(err)
-	// 	}
-	// 	require.Equal(t, http.StatusBadRequest, res.StatusCode)
-	// })
 
-	// t.Run("convert multiple inexistent pdf to jpeg", func(t *testing.T) {
-	// 	defer func() {
-	// 		require.NoError(t, os.RemoveAll("../images"))
-	// 	}()
-	// 	for i := range 5 {
-	// 		body := []byte(fmt.Sprintf(`{"filename": "%d.pdf"}`, i))
-	// 		res, err := http.Post(url, "application/json", bytes.NewReader(body))
-	// 		if err != nil {
-	// 			t.Error(err)
-	// 		}
-	// 		require.Equal(t, http.StatusBadRequest, res.StatusCode)
-	// 	}
-	// })
+	t.Run("convert inexistent pdf to jpeg", func(t *testing.T) {
+		body := []byte(`{"filename": "___.pdf"}`)
+		res, err := http.Post(url, "application/json", bytes.NewReader(body))
+		if err != nil {
+			t.Error(err)
+		}
+		require.Equal(t, http.StatusBadRequest, res.StatusCode)
+	})
+
+	t.Run("convert multiple inexistent pdf to jpeg", func(t *testing.T) {
+		for i := range 5 {
+			body := []byte(fmt.Sprintf(`{"filename": "%d.pdf"}`, i))
+			res, err := http.Post(url, "application/json", bytes.NewReader(body))
+			if err != nil {
+				t.Error(err)
+			}
+			require.Equal(t, http.StatusBadRequest, res.StatusCode)
+		}
+	})
 }
