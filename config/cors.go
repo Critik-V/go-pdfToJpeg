412c3122bc4dc841ae0a64b801aeacc384a78351
yannick2009 <yannickkouakou13@gmail.com>
2024-05-21 16:16:07 +0200

    chores: refactor cors config to adapt it for the server service

--- config/cors.go
+++ config/cors.go
@@ -1,12 +1,17 @@
 package config
 
 import (
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+var serverOrigin string = os.Getenv("SERVER_ORIGIN")
+
 var CorsConfig gin.HandlerFunc = cors.New(cors.Config{
-	AllowOrigins: []string{"*"},
-	AllowMethods: []string{"POST"},
-	AllowHeaders: []string{"Origin"},
+	AllowOrigins:     []string{serverOrigin},
+	AllowMethods:     []string{"POST"},
+	AllowHeaders:     []string{"Origin"},
+	AllowCredentials: true,
 })
