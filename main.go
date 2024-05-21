235ec1efe9a6130c0c0018d59fe4a653734e9a32
yannick2009 <yannickkouakou13@gmail.com>
2024-05-21 16:44:31 +0200

    refactor: update CORS configuration for server service

--- main.go
+++ main.go
@@ -1,31 +1,39 @@
 package main
 
 import (
-	"go-pdf2jpeg/config"
 	"go-pdf2jpeg/handlers"
 	"log"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 const serverPort string = ":5001"
 
-func main() {
+func init() {
 	// Load .env file
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
+}
 
+func main() {
 	if os.Getenv("GIN_MODE") != "" {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	server := gin.Default()
 
 	// Middleware
-	server.Use(config.CorsConfig)
+	var serverOrigin string = os.Getenv("SERVER_SERVICE_ORIGIN")
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{serverOrigin},
+		AllowMethods:     []string{"POST"},
+		AllowHeaders:     []string{"Origin"},
+		AllowCredentials: true,
+	}))
 
 	// Routes
 	server.POST("/convert", handlers.ConvertPdf)
